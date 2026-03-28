3621eabc3765848a332a0ff8ff645332ea5c506f
skyclouds-night <jcmjalquitran@gmail.com>
2026-03-28 12:17:15 +0800

    Initial commit

---
+++ src/main/java/ph/edu/dlsu/greencanyonairbnb/GreenCanyonAirBnBApplication.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.greencanyonairbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenCanyonAirBnBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenCanyonAirBnBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ph/edu/dlsu/greencanyonairbnb/GreenCanyonAirBnBApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.greencanyonairbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenCanyonAirBnBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
