210abcf1c2ec9abc7a514b0634d53097e87cc55d
skyclouds-night <jcmjalquitran@gmail.com>
2026-03-28 18:29:34 +0800

    added property and booked property starter code

--- src/main/java/ph/edu/dlsu/greencanyonairbnb/model/BookedProperty.java
+++ src/main/java/ph/edu/dlsu/greencanyonairbnb/model/BookedProperty.java
@@ -1,17 +1,42 @@
 package ph.edu.dlsu.greencanyonairbnb.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.*;
 
-
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedProperty {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long bookingID;
+
+    @Column(name = "check_In")
     private LocalDate checkInDate;
+
+    @Column(name = "check_Out")
     private LocalDate checkOutDate;
+
+    @Column(name = "guest_name")
     private String guestName;
+
+    @Column(name = "guest_email")
     private String guestEmail;
+    @Column(name = "num_of_Guests")
     private int NumofGuests;
+
+    @Column(name = "confirmation_code")
     private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "property_id")
     private Property property;
 
     public BookedProperty(String bookingConfirmationCode) {
--- src/main/java/ph/edu/dlsu/greencanyonairbnb/model/Property.java
+++ src/main/java/ph/edu/dlsu/greencanyonairbnb/model/Property.java
@@ -1,30 +1,49 @@
 package ph.edu.dlsu.greencanyonairbnb.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
+
 import java.math.BigDecimal;
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Property {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long propertyID;
     private String propertyType;
     private BigDecimal propertyPrice;
     private boolean isBooked = false;
-    //private List<BookedRoom> bookings;
+
+    @Lob
+    private Blob photo;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<BookedProperty> bookings;
 
     public Property() {
-        // this.bookings = new ArrayList<>();
+         this.bookings = new ArrayList<>();
     }
 
-    //public void addBooking(BookedProperty booking) {
-    //if (bookings = null){
-    //bookings = new ArrayList<>();
-}
-//bookings.add(booking);
-// booking.setProperty(this);
-//isBooked = true;
-//String bookingCode = RandomStringUtils.randomNumeric(10);
-//booking.setBookingConfirmationCode(bookingCode);
+    public void addBooking(BookedProperty booking) {
+    if (bookings = null){
+    bookings = new ArrayList<>();
+    }
+    bookings.add(booking);
+     booking.setProperty(this);
+    isBooked = true;
+    String bookingCode = RandomStringUtils.randomNumeric(10);
+    booking.setBookingConfirmationCode(bookingCode);
 
-//}
-//}
+    }
+}
