6d06e7699332ebc7163eaa436687fa575817345f
skyclouds-night <jcmjalquitran@gmail.com>
2026-04-04 18:39:04 +0800

    added over, hate it here

--- src/main/java/ph/edu/dlsu/greencanyonairbnb/AppLauncher.java
+++ src/main/java/ph/edu/dlsu/greencanyonairbnb/AppLauncher.java
@@ -2,6 +2,7 @@ package ph.edu.dlsu.greencanyonairbnb;
 
 import javafx.application.Application;
 
+
 public class AppLauncher {
     public static void main(String[] args) {
         Application.launch(GreenCanyonAirBnBApplication.class, args);
