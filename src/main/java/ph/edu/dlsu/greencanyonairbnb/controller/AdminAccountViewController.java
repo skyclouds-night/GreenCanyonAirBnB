790cdfe53819c133272db1437f45d1e19b48e458
skyclouds-night <jcmjalquitran@gmail.com>
2026-04-10 22:26:46 +0800

    fixed

--- src/main/java/ph/edu/dlsu/greencanyonairbnb/controller/AdminAccountViewController.java
+++ src/main/java/ph/edu/dlsu/greencanyonairbnb/controller/AdminAccountViewController.java
@@ -42,8 +42,8 @@ public class AdminAccountViewController {
     }
 
     @FXML
-    private void goToModify(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/fxml/EditPropertyView.fxml"));
+    private void goToAdminView(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/fxml/HostView.fxml"));
         switchScene(event);
     }
 
